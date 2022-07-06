302dff0d5bae35fe7270b5384aab5bdaa60b4727
Gayatri <gayatri2889@gmail.com>
2022-07-06 23:11:08 +0200

    content filler for about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,7 +1,38 @@
 import React from "react";
+import { Grid ,Container, Card, CardMedia, CardContent, Typography, CardActions, Box} from '@mui/material';
 
 const About = () => {
-    return <h1>About Page</h1>
+    return (
+        <Container fixed>
+            <Grid container spacing={2}>
+                <Grid item xs={12} sm={12} md={12}>
+                    <h1>About</h1>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                        <Typography>
+                        What is Lorem Ipsum?
+
+                        </Typography>
+                        <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                    </Box>
+                </Grid>
+                <Grid item xs={12} sm={6} md={6}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                        <Typography>
+                        What is Lorem Ipsum?
+
+                        </Typography>
+                        <p>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                        </p>
+                    </Box>
+                </Grid>
+            </Grid>
+        </Container>
+    )
 }
 
 export default About;
