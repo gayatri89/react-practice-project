eb37544b6338f18a401afd2d3ae03924e094ae5b
Gayatri <gayatri2889@gmail.com>
2022-07-07 18:25:41 +0200

    created footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
     return(
         <header className="App-footer">
             <ul className="menu-nav">
-                <li>Developed by <Link to="/"> Gayatri Rathva</Link></li>
+                <li>Developed by <a href="https://www.linkedin.com/in/gayatri-rathva-72394734/" target="_blank"> Gayatri Rathva</a></li>
             </ul>
         </header>
     );
--- src/components/example-array/ArrayFunc.js
+++ src/components/example-array/ArrayFunc.js
@@ -4,13 +4,7 @@ import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const ArrayFunc = () => {
 
-    const [expanded, setExpanded] = useState(false);
     const fruits = ['Apple','Banana','Orange','Strawberry','Cherry','Chikoo','Pinapple'];
-
-
-    const handleChange = (panel) => (event, isExpanded) => {
-        setExpanded(isExpanded ? panel : false);
-      };
 
     const arr = () => {
         return fruits.length;
@@ -45,50 +39,51 @@ const ArrayFunc = () => {
         <Container fixed>
 
             <h1>Array Practice</h1>
-            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+            
+
+            <Accordion>
                 <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel1a-content"
-                id="panel1a-header"
+                     expandIcon={<ExpandMoreIcon />}
+                     aria-controls="panel1a-content"
+                     id="panel1a-header"
                 >
-                    <Typography>Array</Typography>
+                    <Typography>Create an array</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>This section provides some examples of common array operations in JavaScript.
-                    <pre>
-                        <code>
-                           {
-                               // 'fruits' array created using array literal notation.""
-                               "const fruits = ['Apple','Banana','Orange','Strawberry','Cherry','Chikoo','Pinapple'];"
-                                //printing the array size
-                            // console.log(fruits.length)
-                           }
-                        </code>
-                    </pre>
+                    <pre class="brush: js notranslate"><code><span class="token comment">// 'fruits' array created using array literal notation.</span>
+<span class="token keyword">const</span> fruits <span class="token operator">=</span> <span class="token punctuation">[</span><span class="token string">'Apple'</span><span class="token punctuation">,</span> <span class="token string">'Banana'</span><span class="token punctuation">]</span><span class="token punctuation">;</span>
+console<span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span>fruits<span class="token punctuation">.</span>length<span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token comment">// 2</span>
+
+<span class="token comment">// 'fruits' array created using the Array() constructor.</span>
+<span class="token keyword">const</span> fruits <span class="token operator">=</span> <span class="token keyword">new</span> <span class="token class-name">Array</span><span class="token punctuation">(</span><span class="token string">'Apple'</span><span class="token punctuation">,</span> <span class="token string">'Banana'</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+console<span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span>fruits<span class="token punctuation">.</span>length<span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token comment">// 2</span>
+
+<span class="token comment">// 'fruits' array created using String.prototype.split().</span>
+<span class="token keyword">const</span> fruits <span class="token operator">=</span> <span class="token string">'Apple, Banana'</span><span class="token punctuation">.</span><span class="token function">split</span><span class="token punctuation">(</span><span class="token string">', '</span><span class="token punctuation">)</span><span class="token punctuation">;</span>
+console<span class="token punctuation">.</span><span class="token function">log</span><span class="token punctuation">(</span>fruits<span class="token punctuation">.</span>length<span class="token punctuation">)</span><span class="token punctuation">;</span>
+<span class="token comment">// 2</span>
+</code></pre>
                     </Typography>
                 </AccordionDetails>
-            </Accordion>
-
-            <Accordion expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
+                </Accordion>
+                <Accordion disabled>
                 <AccordionSummary
-                expandIcon={<ExpandMoreIcon />}
-                aria-controls="panel3bh-content"
-                id="panel3bh-header"
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel2a-content"
+                    id="panel2a-header"
                 >
-                <Typography sx={{ width: '33%', flexShrink: 0 }}>
-                    React Route
-                </Typography>
-                <Typography sx={{ color: 'text.secondary' }}>
-                React Router v6 is Here
-                </Typography>
+                    <Typography>Accordion 2</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                <Typography>
-                    Nunc vitae orci ultricies, auctor nunc in, volutpat nisl. Integer sit
-                    amet egestas eros, vitae egestas augue. Duis vel est augue.
-                </Typography>
+                    <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                    </Typography>
                 </AccordionDetails>
-            </Accordion>
+                </Accordion>
 
      
         </Container>
